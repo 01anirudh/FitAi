ddf8799a9760ba4909e44bbd7f9bc45a391e4bf1
01Anirudh <anirudhkandwal123@gmail.com>
2026-03-28 21:55:02 +0530

    h

--- frontend/config.js
+++
@@ -1,9 +0,0 @@
-// FitAI Frontend Configuration
-// Modify these values to match your local environment
-
-const CONFIG = {
-  gatewayUrl: "http://localhost:9090",         // Spring Cloud Gateway port
-  keycloakUrl: "http://localhost:8080",         // Keycloak server
-  keycloakRealm: "fitness",                     // Keycloak realm name
-  keycloakClientId: "fitness-app",              // Your Keycloak client ID (update if different)
-};
